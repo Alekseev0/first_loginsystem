ea4e437f82146734fa75e8d92b5179dfe3d8d8ac
Alekseev0 <rain123567@gmail.com>
2021-03-25 14:17:54 +0200

    another version

--- rostik.py
+++ rostik.py
@@ -1,63 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['SECRET_KEY'] = 'Thisissecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\rain1\SQLite3\TestDB.db'
+app.config['SECRET_KEY'] = 'Thisissecretkey111'
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\rain1\SQLite3\database.db'
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
+    name = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
 
 
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email, password=password).first()
         if user:
-            if user.password == form.password.data:
-                return redirect(url_for('greet'))
-        return '<h1>Invalid username of password</h1>'
-    return render_template("index.html", form=form)
+            return redirect(url_for('greet'))
+        if not user:
+            msg = 'User is not created'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+
+    return render_template("index.html", msg=msg)
 
 
 @app.route("/registration", methods=['GET', 'POST'])
 def registration():
-    form = RegisterForm()
+    msg = ''
+    if request.method == 'POST' and 'userName' in request.form and 'password' in request.form and 'email' in request.form:
+        name = request.form.get('userName')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
 
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
+        if user:
+            msg = 'Account already exists!'
+        else:
+            new_user = User(name=name, email=email, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+    return render_template('registration.html', msg=msg)
 
-        return redirect(url_for('greet'))
-        #return '<h1>' + form.username.data + '' + form.email.data + '' + form.password.data + '</h1>'
-    return render_template("registration.html", form=form)
+
 
 
 @app.route("/login")
